be6d3294ee7a112e8063f2591595d04d12542a38
Alwin George <alwingeorge11@gmail.com>
2022-04-19 17:27:05 +0930

    resolving refresh issue with heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser')
 const app = express();
 const path = require('path')
-require('dotenv').config({path: './.env'})
+require('dotenv').config({ path: './.env' })
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,8 +10,16 @@ const PORT = process.env.PORT || 3001;
 app.use(cookieParser())
 app.use(express.static("./client/build"));
 
-// app.get("/*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+}
+
+// isProduction &&
+//   app.get("*", function (request, response) {
+//     response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 //   });
 
 const pollsController = require("./controllers/polls");
@@ -19,7 +27,7 @@ const testController = require("./controllers/test")
 const usersController = require("./controllers/users")
 
 app.listen(PORT, () => {
-    console.log('server started on port',PORT)
+    console.log('server started on port', PORT)
 })
 
 
